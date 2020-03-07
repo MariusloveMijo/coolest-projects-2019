9289fc064886da89d59558043ab396e67071fb90
Mikollette <briana.prejoianu@gmail.com>
2020-03-07 16:32:07 +0200

    updated pages

--- src/router.js
+++ src/router.js
@@ -2,14 +2,15 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "./pages/Home.vue";
 import Register from "./pages/Register.vue";
-import Habits from "./pages/Habits.vue";
+import Habit from "./pages/Habit.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
 	{ path: "/", component: Home },
+	{ path: "/habit/:id", component: Habit },
 	{ path: "/register", component: Register },
-	{ path: "/habits", component: Habits }
+	{ path:"/login", component: Login}
 ];
 
 export default new VueRouter({
